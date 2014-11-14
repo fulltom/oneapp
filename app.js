b5f4e272d045f474dd3ecee1fe0284723d06927b
fulltom <vergnaudt@gmail.com>
2014-11-14 13:50:57 +0100

    features : add connect remove jade + update

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-module.exports = function() {
-  	
-}
+	// var express = require("express"),
+	// 	swig = require('swig'),
+	// 	app = express()
+	// ;
+
+	// app.engine('html', swig.renderFile);
+	// app.set('view engine', 'html');
+	// app.set('views', __dirname + '/app/views');
+	// app.set('view cache', false);
+	// swig.setDefaults({ cache: false });
+	// app.use(require('connect-livereload')());
+	// app.use(express.urlencoded());
+	// app.use(express.json());
+	// app.use(express.static(__dirname + '/public'));
+
+	// app.get('/', function (req, res) {
+	// 	res.render('index');
+	//   });
+	// });
+
+	// app.listen(3000);
+	// console.log('Application Started on http://localhost:3000/');
--- gulpfile.js
+++ gulpfile.js
@@ -5,77 +5,68 @@ var gulp = require('gulp'),
 	notify = require('gulp-notify'),
 	concat = require('gulp-concat'),
 	cache = require('gulp-cache'),
+	server = require('gulp-express'),
 	imagemin = require('gulp-imagemin'),
 	minifycss = require('gulp-minify-css'),
 	rename = require('gulp-rename'),
-	inject = require('gulp-inject'),
 	livereload = require('gulp-livereload'),
-    del = require('del');
+	connect = require('gulp-connect'),
+    del = require('del')
 ;
 
-gulp.task('express', function () {
-	var express = require("express"),
-		swig = require('swig'),
-		app = express()
-	;
-	app.engine('html', swig.renderFile);
-	app.set('view engine', 'html');
-	app.set('views', __dirname + '/app/views');
-	app.set('view cache', false);
-	swig.setDefaults({ cache: false });
-	app.use(express.urlencoded())
-	app.use(express.json())
-	app.use(express.static(__dirname + '/public'));
-
-	app.get('/', function (req, res) {
-		res.render('index', {
-			title: 'Consolidate.js'
-	  });
-	});
-
-	app.listen(3000);
-	console.log('Application Started on http://localhost:3000/');
+//Connect
+gulp.task('connect', function() {
+  connect.server({
+    root: 'assets',
+    livereload: true
+  });
+});
+//Copy html
+gulp.task('html', function () {
+  gulp.src('assets/*.html')
+    .pipe(connect.reload())
+    .pipe(gulp.dest('build'))
 });
 
 
 gulp.task('styles', function() {
-  return gulp.src('app/styles/*.scss')
+  return gulp.src('assets/styles/*.scss')
 	.pipe(sass())
 	.pipe(rename({suffix: '.min'}))
 	.pipe(minifycss())
-	.pipe(gulp.dest('dist/css'))
+	.pipe(gulp.dest('build/css'))
 });
 
 gulp.task('scripts', function() {
-  return gulp.src('app/scripts/*.js')
+  return gulp.src('assets/scripts/*.js')
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest('assets/js'))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
-    .pipe(rename({suffix: Date.now()}))
-    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest('build/js'))
 });
 
 gulp.task('images', function() {
-  return gulp.src('app/images/*')
+  return gulp.src('assets/images/*')
     .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-    .pipe(gulp.dest('dist/img'))
+    .pipe(gulp.dest('build/img'))
 });
 
 gulp.task('watch', function() {
-  gulp.watch('app/styles/*.scss', ['styles']);
-  gulp.watch('app/scripts/*.js', ['scripts']);
-  gulp.watch('app/images/*', ['images']);
+	gulp.watch(['assets/*.html'], ['html']);
+	gulp.watch('assets/styles/*.scss', ['styles']);
+	gulp.watch('assets/scripts/*.js', ['scripts']);
+	gulp.watch('assets/images/*', ['images']);
 });
 
 gulp.task('clean', function(cb) {
-    del(['dist/*'], cb);	
+    del(['build/*'], cb);
 });
 
-gulp.task('default', ['express','clean','scripts','styles', 'images', 'watch'], function() {});
+gulp.task('default', ['connect', 'html','scripts','styles', 'images', 'watch','clean'], function() {});
 
-// Create LiveReload server
-livereload.listen();
+// // Create LiveReload server
+// livereload.listen();
 
-gulp.watch(['dist/*']).on('change', livereload.changed);
+// gulp.watch(['dist/*']).on('change', livereload.changed);
 
